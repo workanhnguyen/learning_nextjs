acd3394db502db4937a457a2a8c196fbce2575d6
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-10-29 15:15:48 +0700

    feat: implement ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import { HeroSection } from '@/components'
+import { CustomFilter, HeroSection, SearchBar } from "@/components";
 
 const HomePage = () => {
   return (
-    <main className='overflow-hidden'>
+    <main className="overflow-hidden">
       <HeroSection />
-    </main>
-  )
-}
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
 
-export default HomePage
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            {/* <CustomFilter title="fuel" />
+            <CustomFilter title="year" /> */}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
