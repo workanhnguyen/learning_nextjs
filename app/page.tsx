54e579748d8b91f1a02f0eb943691f85ddc0678f
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-10-29 16:37:43 +0700

    feat: implement car & car details

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-import { CustomFilter, HeroSection, SearchBar } from "@/components";
+import { CarCard, CustomFilter, HeroSection, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <HeroSection />
@@ -19,6 +24,20 @@ const HomePage = () => {
             <CustomFilter title="year" /> */}
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => <CarCard car={car} />)}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+
       </div>
     </main>
   );
---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+  params: { model: "corolla" },
+  headers: {
+    "X-RapidAPI-Key": "97b0fd6fb6msh1265d0d4935bce2p12e79fjsnc0e81438e91a",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+export const fetchCars = async () => (await axios.request(options)).data;
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
